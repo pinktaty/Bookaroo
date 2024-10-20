5d09ae5a94383c6bd678f04ba7a290894e9b44d6
pinktaty <diaz.lilith04@gmail.com>
2024-10-20 10:58:32 -0600

    code v2

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,20 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        playwrite: ["Playwrite GB S", "cursive"],
+        teachers: ["Teachers", "sans-serif"]
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        'ligth-purple': '#C1AAFF',
+        'light-yellow-white': '#FFF3CC',
+        'bright-purple': '#652FFF',
+        'yelloww': '#FFD766',
+        'light-grey': '#F2F2F2',
+        'dark-purple': '#5008BF',
+        'bright-orange': '#FFB700'
       },
     },
   },
